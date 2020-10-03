3449cfc132f182e54de696739fc2cb12a042758d
DoNnMyTh <amit2cha@gmail.com>
2020-10-03 17:14:03 +0530

    Changes involving php

--- project1/Contact.php
+++ project1/Contact.php
@@ -1,11 +1,11 @@
 <?php
 $name = $_Post['name'];
-	$visitor_email = $_Post['email'];
-	$message = $_Post['message'];
+$visitor_email = $_Post['email'];
+$message = $_Post['message'];
 
-$email_body = "User Name: $name.\n"	
-		"User Email: $visitor_email.\n"
-				"User Message: $message.\n";
+$email_body  =  "User Name: $name.\n";
+$email_body .=  "User Email: $visitor_email.\n";
+$email_body .=  "User Message: $message.\n";
 
 
 use PHPMailer\PHPMailer\PHPMailer;
@@ -13,23 +13,23 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 
 // Load Composer's autoloader
-require 'vendor/autoload.php';
+require 'php/autoload.php';
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                    
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'admin.theroasters.me';                   
-    $mail->SMTPAuth   = true;                                 
-    $mail->Username   = 'gjha@theroasters.me';                   
-    $mail->Password   = '';                               
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;        
-    $mail->Port       = 465;                                   
+    $mail->Host       = 'admin.theroasters.me';
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'gjha@theroasters.me';
+    $mail->Password   = '';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 465;
 
-$mail->setFrom('kingghanshyam38@gmail.com', 'Ghanshyam jha');
+    $mail->setFrom('gjha@theroasters.me', 'Ghanshyam jha');
 
 
     // Content
@@ -38,9 +38,7 @@ $mail->setFrom('kingghanshyam38@gmail.com', 'Ghanshyam jha');
     $mail->Body    = $email_body;
 
 
- header("location: message.html");
-
+    header("location: message.html");
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
-?>
